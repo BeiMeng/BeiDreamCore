de9e672d7e01a18b4c60b7fc26c732d91e55f19a
赵亮 <331341164@163.com>
2017-08-17 16:06:06 +0800

    添加项目文件。

---
+++ src/BeiDreamCore.Util/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeiDreamCore
+{
+    public class Class1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Max(List<int> list)
+        {
+            return list.Max();
+        }
+    }
+}
